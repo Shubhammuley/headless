6ebb7f88b842ef2e3637b719062372c63c4effa8
Shubham Muley <tejas.vora@rapidops.com>
2022-09-13 10:49:38 +0530

    Updated

--- src/enviornment/index.js
+++ src/enviornment/index.js
@@ -1,6 +1,6 @@
 const ENV = "development";
 const config = require('./' + ENV);
-const apiEnv = process.env.NODE_ENV === 'production' ? 'netlify_lambda' : 'gatsby_url';
+const apiEnv = 'gatsby_url';
 const apiUrl = config[apiEnv]
 module.exports = {
     apiUrl,
